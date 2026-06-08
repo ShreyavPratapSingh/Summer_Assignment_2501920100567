cbfa144f0a19d332e57f8110a0fe22537582aa3c
ShreyavPratapSingh <kanhaiyapratap1311@gmail.com>
2026-06-09 02:06:08 +0530

    Add files via upload

---
+++ Day_8/Q.31 C++.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows;
+    cout << "Enter total rows: ";
+    cin >> rows;
+    
+    for (int i = 1; i <= rows; i++) {
+        char ch = 'A';
+        for (int j = 1; j <= i; j++) {
+            cout << ch;
+            ch++;
+        }
+        cout << endl;
+    }
+    return 0;
+}
